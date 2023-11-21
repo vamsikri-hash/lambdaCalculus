be37222644084f877a5bc90f129fe8d8d14c2daa
Vamsi Bellam <bellamvamsikrishna3415@gmail.com>
2023-11-21 01:16:16 -0500

    Figure out free variables for a LC expression

--- lib/lci.ml
+++ lib/lci.ml
@@ -4,3 +4,12 @@ let parse s =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
   ast
+
+let free_variables s =
+  let rec free_helper acc = function
+    | Var x -> x :: acc
+    | Abstraction (x, e) ->
+        List.filter (fun ele -> ele <> x) (free_helper acc e)
+    | Application (e1, e2) -> free_helper acc e1 @ free_helper acc e2
+  in
+  s |> parse |> free_helper []
